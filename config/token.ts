abe237e3be9619484dae2e9aecf4eb7730d2f078
Lorcann Rauzduel <contact@lorcannrauzduel.fr>
2024-07-15 07:26:50 +0200

    added some comments

--- config/token.ts
+++ config/token.ts
@@ -1,8 +1,7 @@
 export const options = {
-	decimals: 9,
-	disableFreeze: false,
-	rpcUrl: 'https://api.devnet.solana.com', // https://api.mainnet-beta.solana.com for mainnet
-	uploadMetadata: null,
-	metadataFile: 'metadata.json',
-	totalSupply: 1000000,
+	decimals: 9, // Nombre de décimales du token
+	disableFreeze: false, // Si vrai, ça veut dire que le Freeze Authority sera null, donc il n'y aura pas de possibilité de geler les comptes qui détiendront des tokens
+	rpcUrl: 'https://api.devnet.solana.com', // https://api.mainnet-beta.solana.com pour le mainnet
+	metadataFile: 'metadata.json', // Le nom du fichier JSON qui contient les métadonnées du token (dans le dossier /assets)
+	totalSupply: 1000000, // Nombre total de tokens à créer
 };
